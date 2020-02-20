b4f0b3dab013d44500b73916cb272203e71ed400
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-02-20 08:22:19 +0500

    feat: create solution recursively

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+module.exports = function towelSort (matrix = []) {
+  const matrixLength = matrix.length;
+  let result = [];
 
-// You should implement your task here.
+  if (matrixLength === 0) {
+    return [];
+  }
 
-module.exports = function towelSort (matrix) {
-  return [];
+  if (!Array.isArray(matrix[0])) {
+      return matrix;
+  }
+
+  return  result.concat(matrix.shift())
+                .concat(matrixLength > 1 ? matrix.shift().reverse() : [])
+                .concat(towelSort(matrix));
 }
